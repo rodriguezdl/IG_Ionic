a50ecc99b47abe5d2f532aed6e11b96bb33903e2
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-06-15 03:29:17 -0400

    Fixed syntax issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,39 +11,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent implements OnInit {
   public selectedIndex = 0;
-  // public appPages = [
-  //   {
-  //     title: 'Inbox',
-  //     url: '/folder/Inbox',
-  //     icon: 'mail'
-  //   },
-  //   {
-  //     title: 'Outbox',
-  //     url: '/folder/Outbox',
-  //     icon: 'paper-plane'
-  //   },
-  //   {
-  //     title: 'Favorites',
-  //     url: '/folder/Favorites',
-  //     icon: 'heart'
-  //   },
-  //   {
-  //     title: 'Archived',
-  //     url: '/folder/Archived',
-  //     icon: 'archive'
-  //   },
-  //   {
-  //     title: 'Trash',
-  //     url: '/folder/Trash',
-  //     icon: 'trash'
-  //   },
-  //   {
-  //     title: 'Spam',
-  //     url: '/folder/Spam',
-  //     icon: 'warning'
-  //   }
-  // ];
-  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   public labels = ['John','Jerry','Elise','Elo', 'Thais']
 
 
