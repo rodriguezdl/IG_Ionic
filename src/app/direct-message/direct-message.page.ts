6c094e18421b1bf87f2548cc65bd2c5e8781f653
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-06-17 20:14:00 -0400

    Added msg bubble via styling, added functionality to add to text thread

--- src/app/direct-message/direct-message.page.ts
+++ src/app/direct-message/direct-message.page.ts
@@ -6,21 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./direct-message.page.scss'],
 })
 export class DirectMessagePage implements OnInit {
-  public users =['Daniel','Yadu','E for Pres.','Jon','Darren']
+  public users = ['Daniel', 'Yadu', 'E for Pres.', 'Jon', 'Darren', 'Vasu']
+  public newMsg:any;
 
   messages = [
-  {
-    user: 'Daniel',
-    createdAt: 155409085600,
-    msg: 'Whats up?'
-  },
-  {
-    user: 'E for Pres.',
-    createdAt: 155409085600;
-    msg: 'nm'
-  }
-]
+    {
+      user: 'Daniel',
+      createdAt: 155409085600,
+      msg: 'Whats up?'
+    },
+    {
+      user: 'E for Pres.',
+      createdAt: 155409085600,
+      msg: 'nm'
+    },
+    {
+      user: 'Daniel',
+      createdAt:155409085600,
+      msg: 'Wanna play some soccer?'
+    },
+    {
+      user:'Daniel',
+      createdAt: 155409085600,
+      newMsg: 'This is the message'
+    }
+  ];
 
+  currentUser = 'Daniel';
+
+  sendMessage() {
+      this.messages.push({
+        user: 'Daniel',
+        createdAt: 155409085600,
+        msg:this.newMsg
+      });
+    }
+    
+  
+  
   constructor() { }
 
   ngOnInit() {
