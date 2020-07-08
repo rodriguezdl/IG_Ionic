d6fbb33e78ea7e3a94f6c6ec1a6661b82ecba7b8
sw4rm <ytpillai@pm.me>
2020-07-08 05:28:53 -0400

    Created basic observable in service
    Signed-off-by: sw4rm <ytpillai@pm.me>

--- src/app/direct-message/direct-message.page.ts
+++ src/app/direct-message/direct-message.page.ts
@@ -1,47 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-direct-message',
-  templateUrl: './direct-message.page.html',
-  styleUrls: ['./direct-message.page.scss'],
+    selector: 'app-direct-message',
+    templateUrl: './direct-message.page.html',
+    styleUrls: ['./direct-message.page.scss'],
 })
 export class DirectMessagePage implements OnInit {
-  public users = ['Daniel', 'Yadu', 'E for Pres.', 'Jon', 'Darren', 'Vasu']
-  public newMsg:any;
+    public users = ['Daniel', 'Yadu', 'E for Pres.', 'Jon', 'Darren', 'Vasu'];
+    public newMsg: any;
 
-  messages = [
-    {
-      user: 'Daniel',
-      createdAt: 155409085600,
-      msg: 'Whats up?'
-    },
-    {
-      user: 'E for Pres.',
-      createdAt: 155409085600,
-      msg: 'nm'
-    },
-    {
-      user: 'Daniel',
-      createdAt:155409085600,
-      msg: 'Wanna play some soccer?'
-    },
-  ];
+    messages = [
+        {
+            user: 'Daniel',
+            createdAt: 155409085600,
+            msg: 'Whats up?'
+        },
+        {
+            user: 'E for Pres.',
+            createdAt: 155409085600,
+            msg: 'nm'
+        },
+        {
+            user: 'Daniel',
+            createdAt: 155409085600,
+            msg: 'Wanna play some soccer?'
+        },
+    ];
 
-  currentUser = 'Daniel';
+    currentUser = 'Daniel';
 
-  sendMessage() {
-      this.messages.push({
-        user: 'Daniel',
-        createdAt:155409085600,
-        msg:this.newMsg,
-      });
+    sendMessage() {
+        this.messages.push({
+            user: 'Daniel',
+            createdAt: 155409085600,
+            msg: this.newMsg,
+        });
     }
-    
-  
-  
-  constructor() { }
 
-  ngOnInit() {
-  }
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
