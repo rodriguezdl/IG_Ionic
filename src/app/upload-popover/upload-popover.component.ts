20aab211975c6643df04385db4b0441cf8c5ac25
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-07-21 20:37:05 -0400

    upload component

--- src/app/upload-popover/upload-popover.component.ts
+++ src/app/upload-popover/upload-popover.component.ts
@@ -13,5 +13,55 @@ export class UploadPopoverComponent implements OnInit {
 
   ngOnInit() { }
 
+<<<<<<< HEAD
 
+=======
+  //       }
+  //     }
+  //   })
+  // }
+  
+  // Handles encoding of img when picked
+  pickImage(sourceType) {
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      // let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  // Call action sheet from OS
+  async selectImage() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Select Image source',
+      buttons: [{
+        text: 'Load from Library',
+        handler: () => {
+          this.pickImage(this.camera.PictureSourceType.PHOTOLIBRARY);
+        }
+      },
+      {
+        text: 'Use Camera',
+        handler: () => {
+          this.pickImage(this.camera.PictureSourceType.CAMERA);
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel'
+      }
+      ]
+    });
+    await actionSheet.present();
+  }
+>>>>>>> upload component
 }
