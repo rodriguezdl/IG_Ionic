0a307f98c7c324115eb9858deb934253e24cc217
sw4rm <ytpillai@pm.me>
2020-07-22 01:36:49 -0400

    BIG CHANGES

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,50 +1,48 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'direct-message',
-    loadChildren: () => import('./direct-message/direct-message.module').then( m => m.DirectMessagePageModule)
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'direct-message',
-    loadChildren: () => import('./direct-message/direct-message.module').then( m => m.DirectMessagePageModule)
-  },
-  {
-    path: 'likes',
-    loadChildren: () => import('./likes/likes.module').then( m => m.LikesPageModule)
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
-  },
-  {
-    path: 'test',
-    loadChildren: () => import('./test/test.module').then( m => m.TestPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+    },
+    {
+        path: 'direct-message',
+        loadChildren: () => import('./direct-message/direct-message.module').then(m => m.DirectMessagePageModule)
+    },
+    {
+        path: 'discover',
+        loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule)
+    },
+    {
+        path: 'direct-message',
+        loadChildren: () => import('./direct-message/direct-message.module').then(m => m.DirectMessagePageModule)
+    },
+    {
+        path: 'likes',
+        loadChildren: () => import('./likes/likes.module').then(m => m.LikesPageModule)
+    },
+    {
+        path: 'profile',
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
+    },
+    {
+        path: 'test',
+        loadChildren: () => import('./test/test.module').then(m => m.TestPageModule)
+    },
+
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
